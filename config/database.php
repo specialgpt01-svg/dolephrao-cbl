14b9dcbae9b226ddb2f4fa820f1b22104d505683
specialgpt01-svg <specialgpt01@gmail.com>
2026-09-02 00:10:56 +0700

    fix: enforce localhost host for MariaDB on Linux

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => (PHP_OS_FAMILY === 'Windows' ? env('DB_HOST', '127.0.0.1') : 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'doleph_db_loft_learn'),
             'username' => env('DB_USERNAME', 'doleph_db_loft_learn'),
